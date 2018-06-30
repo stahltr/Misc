654de8faa29e5962125669d2cd2966d2ad16fa44
stahltr <40709853+stahltr@users.noreply.github.com>
2018-06-30 14:09:30 -0700

    Update and rename arrayMax.cpp to arrayishMax.cpp

--- arrayishMax.cpp
+++ arrayishMax.cpp
@@ -3,9 +3,7 @@
 //
 //Date: 2 11 2018
 //
-//Description: takes as parameters an array of ints and the size of the array and returns the maximum value in the array
-//			assume the array contains at least one element
-//           multiple values are tied for the maximum, you would still return that value
+//Description: removed
 ///////////////////////////////////////////////////////////////
 
 //////////////////
